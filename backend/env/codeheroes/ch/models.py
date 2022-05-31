d917ec9248a4e0f0975a3f16ce332ee2fa18854f
Juan Camilo <juancari2000@hotmail.com>
2022-05-31 18:37:48 -0500

    Tabla de usuario

--- backend/env/codeheroes/ch/models.py
+++ backend/env/codeheroes/ch/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Usuario(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    estrellas = models.IntegerField(default=0)
+    puntaje = models.IntegerField(default=0)
+    nivel_1 = models.CharField(default="", max_length=10) #Se define un nivel por sus estrellas y puntaje así: "estrellas, puntaje" -> "3, 100"
+    nivel_2 = models.CharField(default="", max_length=10)
+    nivel_3 = models.CharField(default="", max_length=10)
+    nivel_4 = models.CharField(default="", max_length=10)
+
+    def __str__(self):
+        return self.user
--- backend/env/codeheroes/ch/views.py
+++ backend/env/codeheroes/ch/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from .serializers import UsuarioSerializer
+from .models import Usuario
+from rest_framework import viewsets
 
 # Create your views here.
+class UsuarioViewSet(viewsets.ModelViewSet):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
+   
