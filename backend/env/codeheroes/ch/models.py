44a6ba590af26bf0c8502bc85807cef3c366610a
Juan Camilo <juancari2000@hotmail.com>
2022-05-31 18:42:45 -0500

    Corrección tabla usuario

--- backend/env/codeheroes/ch/models.py
+++ backend/env/codeheroes/ch/models.py
@@ -5,10 +5,11 @@ class Usuario(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     estrellas = models.IntegerField(default=0)
     puntaje = models.IntegerField(default=0)
+    itemsIds = models.CharField(default="", max_length=10)
     nivel_1 = models.CharField(default="", max_length=10) #Se define un nivel por sus estrellas y puntaje así: "estrellas, puntaje" -> "3, 100"
     nivel_2 = models.CharField(default="", max_length=10)
     nivel_3 = models.CharField(default="", max_length=10)
     nivel_4 = models.CharField(default="", max_length=10)
 
     def __str__(self):
-        return self.user
+        return str(self.user)
